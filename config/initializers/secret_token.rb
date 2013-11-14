21e459585863fedae70a96239d0bbb91150e21c1
morishita <tomohiro.m0219@gmail.com>
2013-11-14 13:32:12 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsPaperclip::Application.config.secret_key_base = 'f526dce8712b7a39557ab1f65a8a98d65f5fb1eaafc19bd67b242f6b0c51a526146282b4ace0118776b0cbb6f4c96092edb33738199c936e605ad5476679c886'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsPaperclip::Application.config.session_store :cookie_store, key: '_rails_paperclip_session'
